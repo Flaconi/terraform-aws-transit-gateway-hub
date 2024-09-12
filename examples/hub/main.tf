6b516ef2098566edfbb1974acf1d8be09af077e7
Sergey Novikov <sergey.novikov@flaconi.de>
2024-09-12 09:54:43 +0200

    Add variable for accepting shared attachments

--- examples/hub/main.tf
+++ examples/hub/main.tf
@@ -11,7 +11,8 @@ module "tgw-hub" {
   aws_account_id_hub       = var.aws_account_id_hub
   aws_account_id_satellite = var.aws_account_id_satellite
 
-  allow_external_principals = true
+  allow_external_principals      = false
+  auto_accept_shared_attachments = "disable"
 
   tags = var.tags
 }
